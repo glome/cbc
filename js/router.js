291908a0508d2ad103b35d56d88f1869bd81a31a
Ferenc Szekely <ferenc.szekely@gmail.com>
2013-10-22 14:51:55 +0300

    removed unnecessary route

--- js/router.js
+++ js/router.js
@@ -3,7 +3,6 @@
  */
 App.Router.map(function()
 {
-  this.route("history", { path: '/history' });
   this.resource("products", { path: '/products/:category' }, function()
   {
     this.route('show', { path: '/show/:product_id' });
