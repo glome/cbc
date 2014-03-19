b50f17f0fb804af26cb98ebc57e4b3e3f7843aa0
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-03-19 16:57:43 +0200

    blank project

--- src/application/bootstrap.php
+++ src/application/bootstrap.php
@@ -1,9 +1,2 @@
 <?php
 
-    if ( !file_exists('../vendor/autoload.php')) {
-        echo 'Please complete "composer install" to run the application.';
-        exit;
-    }
-
-
-    require '../vendor/autoload.php';
--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,11 @@
 <?php
 
-    require '../application/bootstrap.php';
+
+    if ( !file_exists(__DIR__  . '/../../vendor/autoload.php')) {
+        echo 'Please complete "composer install" to run the application.';
+        exit;
+    }
+
+
+    require __DIR__ . '/../../vendor/autoload.php';
+    require __DIR__ . '/../application/bootstrap.php';
