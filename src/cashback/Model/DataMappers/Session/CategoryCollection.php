44337586fd1b829ee0f7e6bd0772fa5054ad5b10
Mārtiņš Tereško <martins.teresko@aurumit.com>
2014-04-16 14:24:54 +0300

    disabling collection caching

--- src/cashback/Model/DataMappers/Session/CategoryCollection.php
+++ src/cashback/Model/DataMappers/Session/CategoryCollection.php
@@ -7,6 +7,8 @@
 
         public function fetch($collection)
         {
+            return false;
+
             if (isset($_SESSION['glome.categories'])) {
                 $collection->import($_SESSION['glome.categories']);
                 return true;
