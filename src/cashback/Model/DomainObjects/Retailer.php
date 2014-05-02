e33556bfebd086a9f3e1eafae65642cdf020956c
Ferenc Szekely <ferenc.szekely@gmail.com>
2014-05-03 01:47:18 +0300

    parse more details from the advertisers received from the API

--- src/cashback/Model/DomainObjects/Retailer.php
+++ src/cashback/Model/DomainObjects/Retailer.php
@@ -6,6 +6,8 @@ class Retailer
 {
     private $id = null;
     private $name = null;
+    private $logo = null;
+    private $url = null;
 
     public function setId($id)
     {
@@ -27,11 +29,33 @@ class Retailer
         return $this->name;
     }
 
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setLogo($logo)
+    {
+        $this->logo = $logo;
+    }
+
+    public function getLogo()
+    {
+        return $this->logo;
+    }
+
     public function getParsedArray()
     {
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'logo' => $this->logo,
+            'url' => $this->url,
         ];
     }
 }
